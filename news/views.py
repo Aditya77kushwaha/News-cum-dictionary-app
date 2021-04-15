b38d0cb7e861c20fb8baf9e631df0e3b9228c708
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-04-15 20:17:54 +0530

    Fetched and parsed data from news api in json format.

--- news/views.py
+++ news/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
+API_KEY = '71f3912639934f71b11f94926c92a122'
+
 
 def index(request):
-    return render(request, "index.html", {})
+    url = f'https://newsapi.org/v2/everything?q=Apple&from=2021-04-15&sortBy=popularity&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    articles = data['articles']
+    return render(request, "news.html", {'articles': articles, 'data': data})
