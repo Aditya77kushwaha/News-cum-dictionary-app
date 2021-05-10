a2bc3c690c7548faecd4d632bfe4437241fde90a
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-05-10 13:56:59 +0530

    Added homescreen.

--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
     path('dictionary/', include('dictionary.urls')),
-    path('search/', include('search.urls'))
+    path('search/', include('search.urls')),
+    path('', views.home, name="home")
 ]
